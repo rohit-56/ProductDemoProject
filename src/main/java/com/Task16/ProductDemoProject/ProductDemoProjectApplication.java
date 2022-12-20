7e49ee60e96831386f73fe943e8f5f53e5c11663
Rohit <rohit.sharma2@hexaviewtech.com>
2022-12-20 17:42:05 +0530

    base-setup

---
+++ src/main/java/com/Task16/ProductDemoProject/ProductDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Task16.ProductDemoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Task16/ProductDemoProject/ProductDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Task16.ProductDemoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
